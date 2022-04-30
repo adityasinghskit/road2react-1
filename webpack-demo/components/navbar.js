5a2c3f200e63d8b53ef01ab5047108bc3ec4b3c7
adityasinghskit <adityasinghskit@gmail.com>
2022-04-30 22:45:16 +0530

    first commit

---
+++ webpack-demo/components/navbar.js
@@ -0,0 +1,20 @@
+
+function navbar(){
+    
+    return `<div id="navbar">
+    <a href="index.html">Home</a>
+    <a href="jewelery.html">Jewelery</a>
+    <a href="electronics.html">Electronics</a>
+    <a href="signup.html">SignUp</a>
+    <a href="Login.html">LoginIn</a>
+</div>`
+
+}
+
+function test(){
+
+}
+
+export default navbar;
+
+// "https://masai-api-mocker.herokuapp.com/auth/register"
---
+++ webpack-demo/src/index.js
@@ -0,0 +1,5 @@
+import navbar from "../components/navbar.js";
+
+import "../styles/navbar.css"
+
+document.querySelector("#header").innerHTML=navbar();
